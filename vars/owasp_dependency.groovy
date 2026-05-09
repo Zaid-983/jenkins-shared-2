31cb200af14ef201f0ceb729b82785973334fd40
Zaid-983 <zaidjamal983@gmail.com>
2026-05-09 14:04:19 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,8 +1,14 @@
-
 def call() {
     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
+        def args = '--scan ./' +
+                   ' --nvdApiKey ' + NVD_API_KEY +    // ← concatenation not interpolation
+                   ' --disableYarnAudit' +
+                   ' --disableNodeAudit' +
+                   ' --data /var/lib/jenkins/caches/dependency-check' +
+                   ' --out ./'
+
         dependencyCheck(
-            additionalArguments: '--scan ./ --nvdApiKey $NVD_API_KEY --disableYarnAudit --disableNodeAudit --data /var/lib/jenkins/caches/dependency-check',
+            additionalArguments: args,
             odcInstallation: 'OWASP'
         )
         dependencyCheckPublisher(
